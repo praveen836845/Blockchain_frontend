f085e05195ac69fd9b0862757fad066bb7d2facd
Praveen <roy200297@gmail.com>
2024-08-21 13:44:01 +0530

    changes

--- src/app/_components/StakingForm/StakeForm.tsx
+++ src/app/_components/StakingForm/StakeForm.tsx
@@ -2,11 +2,13 @@
 
 import { useAccount } from 'wagmi';
 import PrimaryButton from '@/components/buttons/PrimaryButton';
-import { FormEvent, useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ethers } from "ethers";
-import StakedBTCABI from '../../../blockchain/config/ABI.json'; // Adjust the import path as needed
+import StakedBTCABI from '../../../blockchain/config/ABI.json';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const CONTRACT_ADDRESS = '0xCd1B7882fF50772D772995120e8c0f1b4c84fBd4'; // Replace with your actual contract address 
+const CONTRACT_ADDRESS = '0xCd1B7882fF50772D772995120e8c0f1b4c84fBd4';
 
 declare global {
   interface Window {
@@ -14,124 +16,75 @@ declare global {
   }
 }
 
-
 const StakeForm = () => {
     const { isConnected } = useAccount();
-    const [tokens, setTokens] = useState<string>(""); 
+    const [tokens, setTokens] = useState<string>("");
 
     const handleDeposit = useCallback(async () => {
-        // e.preventDefault(); // Prevent default form submission
-
-        // const formData = new FormData(e.currentTarget);
-        // const tokens = parseFloat(formData.get('tokens')?.toString() ?? '0');
-
         if (!tokens || parseFloat(tokens) <= 0) {
-            console.log("Please enter a valid token amount.");
+            toast.warn("Please enter a valid token amount.");
             return;
         }
 
         try {
-            // Use Web3Provider instead of BrowserProvider
             const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-            // Request user accounts and get the signer
             await provider.send("eth_requestAccounts", []);
             const signer = provider.getSigner();
-            console.log("Account:", await signer.getAddress());
-
-            // Create the contract instance
             const contract = new ethers.Contract(CONTRACT_ADDRESS, StakedBTCABI, signer);
-            // console.log(contract);
 
-            console.log("Submitting deposit request...", tokens.toString());
-            // const tokenAmount = ethers.utils.parseUnits(tokens, 18);
+            const tx = await contract.deposit({
+                value: tokens
+            });
 
-            // Call the deposit function with the appropriate token amount
-            // const tx = await contract.deposit(parseInt(tokens.toString(), 18)); // Adjust decimals as needed
-            const  tx  = await contract.deposit({
-                value: tokens  // Sending 0.1 BNB (in wei)
-              });
-
-              
-            await tx.wait(); // Wait for the transaction to be mined
-            setTimeout(async () => {
-                try {
-                    const userAddress = await signer.getAddress();
-                    const sBNBBalance = await contract.balanceOf(userAddress);
-                    console.log('User sBNB Balance After Deposit:', sBNBBalance, ethers.utils.formatEther(sBNBBalance));
-                } catch (error) {
-                    console.error("Error retrieving balance:", error);
-                }
-            }, 1000); 
-            console.log("Tokens deposited successfully!");
+            await tx.wait();
+            toast.success("Tokens deposited successfully!");
         } catch (error) {
             console.error("Error staking tokens:", error);
+            toast.error("Error depositing tokens. Please try again.");
         }
     }, [tokens]);
 
     const handleStake = useCallback(async () => {
         if (!tokens || parseFloat(tokens) <= 0) {
-            console.log("Please enter a valid token amount.");
+            toast.warn("Please enter a valid token amount.");
             return;
         }
-        try {
-            // Use Web3Provider instead of BrowserProvider
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-            // Request user accounts and get the signer
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
             await provider.send("eth_requestAccounts", []);
             const signer = provider.getSigner();
-            console.log("Account:", await signer.getAddress());
-
-            // Create the contract instance
             const contract = new ethers.Contract(CONTRACT_ADDRESS, StakedBTCABI, signer);
 
-            console.log("Submitting stake request...", tokens);
-            // const tokenAmount = ethers.utils.parseUnits(tokens, 18);
-
-            // Call the stake function with the appropriate token amount
             const tx = await contract.stake(tokens);
-
-            await tx.wait(); // Wait for the transaction to be mined
-            console.log("Tokens staked successfully!");
+            await tx.wait();
+            toast.success("Tokens staked successfully!");
         } catch (error) {
             console.error("Error staking tokens:", error);
+            toast.error("Error staking tokens. Please try again.");
         }
-    }
-
-    ,[tokens]);
+    }, [tokens]);
 
     const handleUnstake = useCallback(async () => {
         if (!tokens || parseFloat(tokens) <= 0) {
-            console.log("Please enter a valid token amount.");
+            toast.warn("Please enter a valid token amount.");
             return;
         }
-        try {
-            // Use Web3Provider instead of BrowserProvider
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-            // Request user accounts and get the signer
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
             await provider.send("eth_requestAccounts", []);
             const signer = provider.getSigner();
-            console.log("Account:", await signer.getAddress());
-
-            // Create the contract instance
             const contract = new ethers.Contract(CONTRACT_ADDRESS, StakedBTCABI, signer);
 
-            console.log("Submitting unstake request...", tokens);
-            // const tokenAmount = ethers.utils.parseUnits(tokens, 18);
-
-            // Call the stake function with the appropriate token amount
             const tx = await contract.unstake(tokens);
-
-            await tx.wait(); // Wait for the transaction to be mined
-            console.log("Tokens unstaked successfully!");
+            await tx.wait();
+            toast.success("Tokens unstaked successfully!");
         } catch (error) {
-            console.error("Error staking tokens:", error);
+            console.error("Error unstaking tokens:", error);
+            toast.error("Error unstaking tokens. Please try again.");
         }
-    }
-    ,[tokens]);
-
+    }, [tokens]);
 
     return (
         <div className="my-2 w-[300px]">
@@ -141,12 +94,12 @@ const StakeForm = () => {
                 className="mb-2 w-full rounded-md p-2"
                 placeholder='State amount (in WEI)'
                 min="0"
-                step="any" // Allows fractional values
+                step="any"
                 value={tokens}
                 onChange={(e) => setTokens(e.target.value)}
                 required
             />
-            <PrimaryButton disabled={!isConnected} onClick={handleDeposit}  className="inline-block w-full disabled:cursor-not-allowed">
+            <PrimaryButton disabled={!isConnected} onClick={handleDeposit} className="inline-block w-full disabled:cursor-not-allowed">
                 Buy sBNB
             </PrimaryButton>
             <PrimaryButton disabled={!isConnected} onClick={handleStake} className="inline-block mt-3 w-full disabled:cursor-not-allowed" style={{ backgroundColor: '#FF6347', color: '#fff' }}>
@@ -155,8 +108,8 @@ const StakeForm = () => {
             <PrimaryButton disabled={!isConnected} onClick={handleUnstake} className="inline-block mt-3 w-full disabled:cursor-not-allowed" style={{ backgroundColor: '#FF6347', color: '#fff' }}>
                 UnStake sBNB
             </PrimaryButton>
+            <ToastContainer />
         </div>
-        
     );
 }
 
